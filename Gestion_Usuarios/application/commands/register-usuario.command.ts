6fd5c493a6db977967266e6b5e2551afe7c9f97f
FrancoSurco <85416569+FrancoSurco@users.noreply.github.com>
2022-06-30 04:04:26 -0500

    Delete Gestion_Usuarios directory

--- Gestion_Usuarios/application/commands/register-usuario.command.ts
+++
@@ -1,9 +0,0 @@
-export class RegisterUsuarioCommand {
-  constructor(
-    public readonly name: string,
-    public readonly lastName: string,
-    public readonly email: string,
-    public readonly password: string,
-    //public readonly age: number,
-  ) {}
-}
--- Gestion_Usuarios/application/dtos/request/register-usuario-request.dto.ts
+++
@@ -1,9 +0,0 @@
-export class RegisterUsuarioRequestDto {
-  constructor(
-    public readonly name: string,
-    public readonly lastName: string,
-    public readonly password: string,
-    public readonly email: string,
-    //public readonly age: number
-  ) {}
-}
